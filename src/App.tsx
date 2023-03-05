2beefd9a679171fdc8177a59feb35214ee5fc25c
Byun Kyung MIn <chgemn@gmail.com>
2023-03-06 01:21:41 +0900

    Add Marquee

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,55 @@
 import React from 'react';
 import './css/App.css';
+import Marquee from "react-fast-marquee";
+import styled from "styled-components";
 
-function App() {
+
+const leftTextItems = [
+  "나의 시선들이 듬뿍 담긴 사진들",
+  "한양대학교 건축학부 22학번 변경민",
+  "@chgemn",
+  "지극히 개인적인 시선들",
+  "질문 받아요",
+]
+
+export default function App() {
   return (
     <div>
-      <p>
-        제작중 !! 테스트 7:58분
-      </p>
+      <LeftMarqueeWrapper>
+        <Marquee gradientColor={[19,19,19]} speed={2}>
+          {leftTextItems.map((item) => (
+              <FlowingText>{item}</FlowingText>
+          ))}
+        </Marquee>
+      </LeftMarqueeWrapper>
+
+      <RightMarqueeWrapper>
+        <Marquee gradientColor={[19,19,19]} speed={2}>
+          {leftTextItems.map((item) => (
+              <FlowingText>{item}</FlowingText>
+          ))}
+        </Marquee>
+      </RightMarqueeWrapper>
     </div>
   );
 }
 
-export default App;
+const FlowingText = styled.p`
+  margin: 0px 20px 0px 0px;
+  font-family: "NanumSquareNeo-EB";
+  font-size: clamp(1rem, 10vw, 3rem);
+`
+
+const LeftMarqueeWrapper = styled.div`
+  position: absolute;
+  top: 0%;
+  left: -90vw;
+  transform: rotate(-90deg);
+`
+
+const RightMarqueeWrapper = styled.div`
+  position: absolute;
+  top: 50vh;
+  left: 60vw;
+  transform: rotate(90deg);
+`
