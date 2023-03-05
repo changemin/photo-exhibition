318bd682ebe910b79d5c1fbc5f82b4bfa0d5b1d1
Byun Kyung MIn <chgemn@gmail.com>
2023-03-05 19:50:22 +0900

    install styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          제작중 !! 테스트
+          제작중 !! 테스트 7:19분
         </p>
       </header>
     </div>
