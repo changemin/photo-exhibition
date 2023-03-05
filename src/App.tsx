c3f24ecd7ed2a3ea3f97b2d76c98316ad1ec5f1c
Byun Kyung MIn <chgemn@gmail.com>
2023-03-05 19:59:16 +0900

    Remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          제작중 !! 테스트 7:19분
+          제작중 !! 테스트 7:58분
         </p>
       </header>
     </div>
