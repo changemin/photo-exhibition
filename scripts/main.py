de98132bee3848fe39a58c4a28f55dadf5c430cd
Byun Kyung MIn <chgemn@gmail.com>
2023-03-06 23:14:59 +0900

    add read exif data

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,13 @@
-import config
+from PIL import Image, ExifTags
 
-print(config.username)
-print(config.password)
+img = Image.open("./../photos/photo (1).jpg")
+img_exif = img.getexif()
+print(type(img_exif))
+# <class 'PIL.Image.Exif'>
+
+if img_exif is None:
+    print('Sorry, image has no exif data.')
+else:
+    for key, val in img_exif.items():
+        if key in ExifTags.TAGS:
+            print(f'{ExifTags.TAGS[key]}:{val}')
